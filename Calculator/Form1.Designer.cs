c842b6493150ac7256e484a21b61fc81ce62663e
jhkwon92 <jhkwon92@naver.com>
2017-11-29 21:52:44 +0900

    implement Add and Div

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -32,11 +32,23 @@
             this.NumButton2 = new System.Windows.Forms.Button();
             this.NumScreen = new System.Windows.Forms.Label();
             this.NumPlus = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.NumClear = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // NumButton1
             // 
-            this.NumButton1.Location = new System.Drawing.Point(48, 244);
+            this.NumButton1.Location = new System.Drawing.Point(48, 546);
             this.NumButton1.Margin = new System.Windows.Forms.Padding(4);
             this.NumButton1.Name = "NumButton1";
             this.NumButton1.Size = new System.Drawing.Size(118, 117);
@@ -47,14 +59,10 @@
             // 
             // NumButton2
             // 
-            this.NumButton2.Location = new System.Drawing.Point(201, 244);
-            this.NumButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.NumButton2.Location = new System.Drawing.Point(0, 0);
             this.NumButton2.Name = "NumButton2";
-            this.NumButton2.Size = new System.Drawing.Size(118, 117);
-            this.NumButton2.TabIndex = 1;
-            this.NumButton2.Text = "2";
-            this.NumButton2.UseVisualStyleBackColor = true;
-            this.NumButton2.Click += new System.EventHandler(this.NumButton2_Click);
+            this.NumButton2.Size = new System.Drawing.Size(75, 23);
+            this.NumButton2.TabIndex = 12;
             // 
             // NumScreen
             // 
@@ -64,14 +72,14 @@
             this.NumScreen.Location = new System.Drawing.Point(48, 65);
             this.NumScreen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NumScreen.Name = "NumScreen";
-            this.NumScreen.Size = new System.Drawing.Size(953, 104);
+            this.NumScreen.Size = new System.Drawing.Size(669, 104);
             this.NumScreen.TabIndex = 2;
             this.NumScreen.Text = "0";
             this.NumScreen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // NumPlus
             // 
-            this.NumPlus.Location = new System.Drawing.Point(883, 244);
+            this.NumPlus.Location = new System.Drawing.Point(599, 546);
             this.NumPlus.Margin = new System.Windows.Forms.Padding(4);
             this.NumPlus.Name = "NumPlus";
             this.NumPlus.Size = new System.Drawing.Size(118, 117);
@@ -80,11 +88,155 @@
             this.NumPlus.UseVisualStyleBackColor = true;
             this.NumPlus.Click += new System.EventHandler(this.NumPlus_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 244);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 117);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "9";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 393);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 117);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "4";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(201, 393);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 117);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "5";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(48, 244);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 117);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "7";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(357, 393);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 117);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "6";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(357, 546);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 117);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(201, 244);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(118, 117);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "8";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(201, 682);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(118, 117);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "0";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(201, 546);
+            this.button9.Margin = new System.Windows.Forms.Padding(4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(118, 117);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "2";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(599, 393);
+            this.button10.Margin = new System.Windows.Forms.Padding(4);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(118, 117);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "-";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.NumPlus_Click);
+            // 
+            // NumClear
+            // 
+            this.NumClear.Location = new System.Drawing.Point(599, 244);
+            this.NumClear.Margin = new System.Windows.Forms.Padding(4);
+            this.NumClear.Name = "NumClear";
+            this.NumClear.Size = new System.Drawing.Size(118, 117);
+            this.NumClear.TabIndex = 15;
+            this.NumClear.Text = "c";
+            this.NumClear.UseVisualStyleBackColor = true;
+            this.NumClear.Click += new System.EventHandler(this.NumClear_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(599, 682);
+            this.button11.Margin = new System.Windows.Forms.Padding(4);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(118, 117);
+            this.button11.TabIndex = 16;
+            this.button11.Text = "=";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.NumPlus_Click);
+            // 
             // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 648);
+            this.ClientSize = new System.Drawing.Size(779, 838);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.NumClear);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.NumPlus);
             this.Controls.Add(this.NumScreen);
             this.Controls.Add(this.NumButton2);
@@ -104,6 +256,18 @@
         private System.Windows.Forms.Button NumButton2;
         private System.Windows.Forms.Label NumScreen;
         private System.Windows.Forms.Button NumPlus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button NumClear;
+        private System.Windows.Forms.Button button11;
     }
 }
 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,41 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calculator
 {
+    public enum Operator { Add, Sub, Multi, Div } 
+
     public partial class Calculator : Form
     {
-        //test
         public int Result = 0;
         public bool isNewNum = true;
-
-
-
-        public int Result = 0;
-        public bool isNewNum = true;
-        public int Result = 0;
-        public bool isNewNum = true;
-        public int Result = 0;
-        public bool isNewNum = true;
-        public int Result = 0;
-        public bool isNewNum = true;
-        public int Result = 0;
-        public bool isNewNum = true;
-        public int Result = 0;
-        public bool isNewNum = true;
-        public int Result = 0;
-        public bool isNewNum = true;
+        public Operator Opt = Operator.Add;
 
         public Calculator()
         {
-            InitializeComponent();
+            InitializeComponent(); 
 
         }
 
@@ -46,15 +24,10 @@ namespace Calculator
 
         private void NumButton1_Click(object sender, EventArgs e)
         {
-            
-            SetNum("1");
+            Button numButton = (Button)sender;
+            SetNum(numButton.Text);
         }
 
-        private void NumButton2_Click(object sender, EventArgs e)
-        {
-            SetNum("2");
-        }
-        
         public void SetNum(string num)
         {
             if (isNewNum)
@@ -74,12 +47,34 @@ namespace Calculator
 
         private void NumPlus_Click(object sender, EventArgs e)
         {
-            int num = int.Parse(NumScreen.Text);
-            Result = Result + num;
+            if(isNewNum == false)
+            {
+                int num = int.Parse(NumScreen.Text);
+                if (Opt == Operator.Add)
+                    Result = Result + num;
+                else if (Opt == Operator.Div)
+                    Result = Result - num;
 
-            NumScreen.Text = Result.ToString();
-            isNewNum = true;
+                NumScreen.Text = Result.ToString();
+                isNewNum = true;
+            }
+            
+
+            Button optButton = (Button)sender;
+            if (optButton.Text == "+")
+                Opt = Operator.Add;
+            else if (optButton.Text == "-")
+                Opt = Operator.Div;
+                
         }
 
+        private void NumClear_Click(object sender, EventArgs e)
+        {
+            Result = 0;
+            isNewNum = true;
+            Opt = Operator.Add;
+
+            NumScreen.Text = "0";
+        }
     }
 }
